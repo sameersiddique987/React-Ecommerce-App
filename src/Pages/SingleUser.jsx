e18e3d9da210601f8999b09f1c436c1b2af2609c
sameersiddique987 <sameersiddique301@gmail.com>
2025-02-16 09:59:09 +0500

    Updated

--- src/Pages/SingleUser.jsx
+++ src/Pages/SingleUser.jsx
@@ -115,7 +115,7 @@ function SingleUser() {
       
       const response = await axios({
         method: "post",
-        url: "https://payment-integration-3dhj.vercel.app/api/v1/checkout",
+        url: "https://payment-integration-58rt.vercel.app/api/v1/checkout",
         data: { products: [productToCheckout] },
         withCredentials: false, 
       });
